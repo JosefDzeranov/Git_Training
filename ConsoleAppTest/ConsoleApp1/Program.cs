c883174af466a8b798753bc06d36cf2d4b975d50
JosefDzeranov <soslan29@gmail.com>
2022-12-12 10:06:29 +0300

    Исправил замечания

--- ConsoleAppTest/ConsoleApp1/Program.cs
+++ ConsoleAppTest/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
+        // исправление зщамечаний после ревью
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
