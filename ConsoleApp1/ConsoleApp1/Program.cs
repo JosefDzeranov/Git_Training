b938d8a2b3b7b0b7b2b0627ad370bab5ea0c4a93
JosefDzeranov <soslan29@gmail.com>
2022-12-12 09:31:28 +0300

    Создал новый проект

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Это изменение в branch2
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
